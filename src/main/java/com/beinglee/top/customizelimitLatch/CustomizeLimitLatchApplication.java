7c96b78f1ce67c2238667ed69be55dbad1675fd4
PSDLGD <beingandcream@gmail.com>
2019-12-29 17:31:03 +0800

    first commit

---
+++ src/main/java/com/beinglee/top/customizelimitLatch/CustomizeLimitLatchApplication.java
@@ -0,0 +1,13 @@
+package com.beinglee.top.customizelimitLatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomizeLimitLatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomizeLimitLatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beinglee/top/customizelimitLatch/CustomizeLimitLatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beinglee.top.customizelimitLatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomizeLimitLatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
